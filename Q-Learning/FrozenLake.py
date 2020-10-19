7762f10e80c3a285a9df731046e60165600fc409
valitovrus <valitov.rus@gmail.com>
2020-10-19 10:02:58 +0200

    frozen lake preps

--- Q-Learning/FrozenLake.py
+++ Q-Learning/FrozenLake.py
@@ -1,1 +1,25 @@
 # %%
+import random
+import numpy as np
+import gym
+
+# %%
+frozenLake = gym.make("FrozenLake-v0")
+
+# %%
+actionSize = frozenLake.action_space.n
+stateSize = frozenLake.observation_space.n
+qtable = np.zeros((stateSize, actionSize))
+
+# %%
+epochs = 50*1000
+testEpochs = 100 
+maxSteps = 99
+
+learningRate = 0.7 
+gamma = 0.618 # why? 
+
+epsilon = 1.0 
+startEpsilon = 1.0
+endEpsilon = 0.01 
+decayRate = 0.01 
